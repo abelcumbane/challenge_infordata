85c998ad67e13d6c24af1788064326c8ca9f92b1
Abel Cumbane <abelrosac@gmail.com>
2025-06-20 09:04:43 +0100

    chore: Updates na entidade Fornecedores

--- src/main/java/com/desafio/demo/entitys/Fornecedores.java
+++ src/main/java/com/desafio/demo/entitys/Fornecedores.java
@@ -1,5 +1,95 @@
 package com.desafio.demo.entitys;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Fornecedores {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private Integer nuit;
+	
+	@Column
+	private String telfone;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String endereco;
+	
+	@Column
+	private boolean estado;
+	
+	public Fornecedores() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getNuit() {
+		return nuit;
+	}
+
+	public void setNuit(Integer nuit) {
+		this.nuit = nuit;
+	}
+
+	public String getTelfone() {
+		return telfone;
+	}
+
+	public void setTelfone(String telfone) {
+		this.telfone = telfone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+		
 
 }
