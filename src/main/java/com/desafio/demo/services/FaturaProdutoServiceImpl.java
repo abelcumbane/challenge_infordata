e4ff44f5f5f32e6c77af9761c528a142de56faab
Abel Cumbane <abelrosac@gmail.com>
2025-06-21 15:07:18 +0100

    chore: FaturaProdutoServiceImpl implemeted

---
+++ src/main/java/com/desafio/demo/services/FaturaProdutoServiceImpl.java
@@ -0,0 +1,72 @@
+package com.desafio.demo.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.desafio.demo.entitys.FaturaProduto;
+
+@Service
+public class FaturaProdutoServiceImpl implements FaturaProdutoService{
+
+	@Override
+	public void save(FaturaProduto faturaproduto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(FaturaProduto faturaproduto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(FaturaProduto faturaproduto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<FaturaProduto> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FaturaProduto> find(String substring) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FaturaProduto findById(Integer faturaprodutoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long countFaturaProduto() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public FaturaProduto create(FaturaProduto faturaproduto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<FaturaProduto> findByFacturacaoId(Long facturacaoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
