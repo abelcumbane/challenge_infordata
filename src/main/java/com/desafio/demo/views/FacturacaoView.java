53f9c44300996ddee7b63e6817982483ce589bf5
Abel Cumbane <abelrosac@gmail.com>
2025-06-23 21:43:22 +0100

    Update FacturacaoView.java

--- src/main/java/com/desafio/demo/views/FacturacaoView.java
+++ src/main/java/com/desafio/demo/views/FacturacaoView.java
@@ -11,6 +11,7 @@ import com.desafio.demo.entitys.Clientes;
 import com.desafio.demo.entitys.Facturacao;
 import com.desafio.demo.entitys.FaturaProduto;
 import com.desafio.demo.entitys.Produtos;
+import com.desafio.demo.entitys.enums.EstadoFatura;
 import com.desafio.demo.services.ClienteService;
 import com.desafio.demo.services.FacturacaoService;
 import com.desafio.demo.services.ProdutoService;
@@ -22,6 +23,8 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 import java.time.LocalDate;
@@ -39,6 +42,9 @@ public class FacturacaoView extends VerticalLayout {
     private ComboBox<Produtos> produtoCombo;
     private IntegerField quantidadeField;
     private NumberField totalField;
+    private TextField numeroFaturaField;
+    private ComboBox<EstadoFatura> estadoCombo;
+    private TextArea observacoesField;
     private Grid<FaturaProduto> grid;
 
     private List<FaturaProduto> itens = new ArrayList<>();
@@ -50,8 +56,8 @@ public class FacturacaoView extends VerticalLayout {
         this.clienteService = clienteService;
         this.produtoService = produtoService;
 
-        configureComponents();
         configureGrid();
+        configureComponents();
         buildLayout();
     }
 
@@ -66,6 +72,18 @@ public class FacturacaoView extends VerticalLayout {
 
         quantidadeField = new IntegerField("Quantidade");
         quantidadeField.setMin(1);
+        
+        numeroFaturaField = new TextField("Número da Fatura");
+        numeroFaturaField.setReadOnly(true); // será gerado automaticamente
+        numeroFaturaField.setValue(gerarNumeroFatura());
+
+        estadoCombo = new ComboBox<>("Estado");
+        estadoCombo.setItems(EstadoFatura.values());
+        estadoCombo.setValue(EstadoFatura.EMITIDA); // valor padrão
+
+        observacoesField = new TextArea("Observações");
+        observacoesField.setPlaceholder("Digite observações da fatura (opcional)");
+
 
         totalField = new NumberField("Total");
         totalField.setReadOnly(true);
@@ -74,14 +92,20 @@ public class FacturacaoView extends VerticalLayout {
         Button salvarBtn = new Button("Salvar Fatura", e -> salvarFatura());
 
         HorizontalLayout form = new HorizontalLayout(produtoCombo, quantidadeField, adicionarItemBtn);
-        add(clienteCombo, form, grid, totalField, salvarBtn);
+        VerticalLayout faturaMeta = new VerticalLayout(numeroFaturaField, estadoCombo, observacoesField);
+        add(clienteCombo,faturaMeta, form, grid, totalField, salvarBtn);
     }
 
-    private void configureGrid() {
+    private String gerarNumeroFatura() {
+    	long count = facturacaoService.countFacturacao() + 1;
+        return String.format("F%03d", count);
+	}
+
+	private void configureGrid() {
         grid = new Grid<>(FaturaProduto.class, false);
         grid.addColumn(i -> i.getProduto().getDescricao()).setHeader("Produto");
         grid.addColumn(FaturaProduto::getQuantidade).setHeader("Qtd");
-        grid.addColumn(FaturaProduto::getPreco).setHeader("Preço");
+        grid.addColumn(FaturaProduto::getPrecoUnitario).setHeader("Preço Unitario");
         grid.addColumn(FaturaProduto::getSubtotal).setHeader("Subtotal");
     }
 
@@ -103,7 +127,7 @@ public class FacturacaoView extends VerticalLayout {
         FaturaProduto item = new FaturaProduto();
         item.setProduto(produto);
         item.setQuantidade(quantidade);
-        item.setPreco(produto.getPreco());
+        item.setPrecoUnitario(produto.getPreco());
         item.setSubtotal(produto.getPreco() * quantidade);
         itens.add(item);
 
@@ -127,9 +151,13 @@ public class FacturacaoView extends VerticalLayout {
 
         Facturacao fatura = new Facturacao();
         fatura.setCliente(clienteCombo.getValue());
-        fatura.setData(LocalDate.now());
+        fatura.setDataEmissao(LocalDate.now());
         fatura.setItens(itens); // precisa estar com cascade na entidade
         fatura.setTotal(totalField.getValue());
+        
+        fatura.setNumero(numeroFaturaField.getValue());
+        fatura.setEstado(estadoCombo.getValue());
+        fatura.setObservacoes(observacoesField.getValue());
 
         for (FaturaProduto item : itens) {
             item.setFatura(fatura); // liga o item à fatura
